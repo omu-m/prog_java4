52615e939065221c3a6b6e1135fb4207ec4810eb
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-02-28 12:15:47 +0000

    オブジェクト指向の基本

---
+++ lessons2/Person4_3.java
@@ -0,0 +1,3 @@
+// Personクラスを定義してください
+class Person4_3 {
+}
---
+++ lessons2/study4_3.java
@@ -0,0 +1,9 @@
+class study4_3 {
+  public static void main(String[] args) {
+    // Personクラスのインスタンスを生成し、変数person1に代入してください
+    Person4_3 person1 = new Person4_3();
+
+    // Personクラスのインスタンスを生成し、変数person2に代入してください
+    Person4_3 person2 = new Person4_3();
+  }
+}
