0b8d9c963ac64b20043efdff347729d4a6399934
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-03-01 11:54:07 +0000

    オブジェクト指向の基本

---
+++ lessons2/Person4_4.java
@@ -0,0 +1,6 @@
+class Person4_4 {
+  // インスタンスメソッドhelloを定義してください
+  public void hello() {
+    System.out.println("こんにちは");
+  }
+}
---
+++ lessons2/study4_4.java
@@ -0,0 +1,12 @@
+class study4_4 {
+  public static void main(String[] args) {
+    Person4_4 person1 = new Person4_4();
+    // person1に対して、helloメソッドを呼び出してください
+    person1.hello();
+
+    Person4_4 person2 = new Person4_4();
+    // person2に対して、helloメソッドを呼び出してください
+    person2.hello();
+
+  }
+}
