979b8f8b39a9d51545e93b2e173d6305fc1d388f
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-03-02 11:08:49 +0000

    オブジェクト指向の基本

---
+++ lessons2/Person4_5.java
@@ -0,0 +1,8 @@
+class Person4_5 {
+  // インスタンスフィールドnameを定義してください
+  public String name;
+
+  public void hello() {
+    System.out.println("こんにちは");
+  }
+}
---
+++ lessons2/study4_5.java
@@ -0,0 +1,21 @@
+class study4_5 {
+  public static void main(String[] args) {
+    Person4_5 person1 = new Person4_5();
+    person1.hello();
+    
+    // person1のインスタンスフィールドnameに「Kate Jones」をセットしてください
+    person1.name = "Kate Jones";
+    
+    // person1のインスタンスフィールドnameの値を出力してください
+    System.out.println(person1.name);
+
+    Person4_5 person2 = new Person4_5();
+    person2.hello();
+    
+    // person2のインスタンスフィールドnameに「John Christopher Smith」をセットしてください
+    person2.name = "John Christopher Smith";
+    
+    // person2のインスタンスフィールドnameの値を出力してください
+    System.out.println(person2.name);
+  }
+}
