01c413d228ac941b1fbc026ab864821b41df1ce8
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-03-03 11:21:10 +0000

    オブジェクト指向の基本

---
+++ lessons2/study4_6.java
@@ -0,0 +1,11 @@
+class study4_6 {
+  public static void main(String[] args) {
+    Person4_6 person1 = new Person4_6();
+    person1.name = "Kate Jones";
+    person1.hello();
+
+    Person4_6 person2 = new Person4_6();
+    person2.name = "John Christopher Smith";
+    person2.hello();
+  }
+}
