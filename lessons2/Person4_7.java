6d4ab33d45a4f582aa0d8c39d97a673118bc2c11
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-03-05 11:20:49 +0000

    オブジェクト指向の基本

---
+++ lessons2/Person4_7.java
@@ -0,0 +1,12 @@
+class Person4_7 {
+  public String name;
+
+  // コンストラクタを定義してください
+  Person4_7() {
+    System.out.println("インスタンスが生成されました");
+  }
+
+  public void hello() {
+    System.out.println("こんにちは、私は" + this.name + "です");
+  }
+}
---
+++ lessons2/study4_7.java
@@ -0,0 +1,6 @@
+class study4_7 {
+  public static void main(String[] args) {
+    Person4_7 person1 = new Person4_7();
+    Person4_7 person2 = new Person4_7();
+  }
+}
