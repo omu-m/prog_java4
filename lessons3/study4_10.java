976436daee8582146f3171ffa6051eb3610af5c5
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-03-15 12:12:54 +0000

    自己紹介プログラムを作ろう

---
+++ lessons3/study4_10.java
@@ -0,0 +1,11 @@
+class study4_10 {
+  public static void main(String[] args) {
+    Person4_10 person1 = new Person4_10("Kate", "Jones", 27, 1.6, 50.0);
+
+    System.out.println(person1.firstName);
+    System.out.println(person1.lastName);
+    System.out.println(person1.age);
+    System.out.println(person1.height);
+    System.out.println(person1.weight);
+  }
+}
