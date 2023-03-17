74d267a23d181a8d428aefc87548b94c4a3d9371
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-03-17 08:51:01 +0000

    自己紹介プログラムを作ろう

---
+++ lessons3/Person4_12.java
@@ -0,0 +1,30 @@
+class Person4_12 {
+  public String firstName;
+  public String lastName;
+  public int age;
+  public double height;
+  public double weight;
+
+  Person4_12(String firstName, String lastName, int age, double height, double weight) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+
+  public String fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  public double bmi() {
+    return this.weight / this.height / this.height;
+  }
+  
+  // printDataメソッドを定義してください
+  public void printData() {
+    System.out.println("私の名前は" + this.fullName() + "です");
+    System.out.println("年齢は" + this.age + "歳です");
+    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
+  }
+}
---
+++ lessons3/study4_12.java
@@ -0,0 +1,6 @@
+class study4_12 {
+  public static void main(String[] args) {
+    Person4_12 person1 = new Person4_12("Kate", "Jones", 27, 1.6, 50.0);
+    person1.printData();
+  }
+}
